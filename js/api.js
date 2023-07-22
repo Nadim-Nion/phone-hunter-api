79a924b6305a2fe7cbfe7911f36de5d5b3b861ee
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2023-07-22 19:48:52 +0600

    selected phone details

--- js/api.js
+++ js/api.js
@@ -18,7 +18,7 @@ const displaySearchResult = phones => {
     // console.log(phones);
     const searchResult = document.getElementById('search-result');
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -38,5 +38,47 @@ const displaySearchResult = phones => {
 
 
 const loadPhoneDetail = phoneID => {
-    console.log(phoneID);
+    // console.log(phoneID);
+    const url = ` https://openapi.programming-hero.com/api/phone/${phoneID}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data));
+}
+
+
+const displayPhoneDetail = phone => {
+    // console.log(phone);
+    const phoneDetail = document.getElementById('phone-detail');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${phone.name}</h5>
+            <p class="card-text">
+                Brand: ${phone.brand} <br>
+                Release Date: ${phone.releaseDate} <br>
+                <b> Main Features: <br> </b>
+                    Display Size: ${phone.mainFeatures.displaySize} <br>
+                    Memory: ${phone.mainFeatures.memory} <br>
+                    Chip Set:  ${phone.mainFeatures.chipSet} <br>
+                <b> Sensors: <br> </b>
+                     ${phone.mainFeatures.sensors[0]} <br>
+                     ${phone.mainFeatures.sensors[1]} <br>
+                     ${phone.mainFeatures.sensors[2]} <br> 
+                     ${phone.mainFeatures.sensors[3]} <br>
+                     ${phone.mainFeatures.sensors[4]} <br>
+                     ${phone.mainFeatures.sensors[5]} <br>
+
+                <b> Others: <br> </b>
+                  GPS: ${phone.others ? phone.others.GPS : 'Not found'} <br>
+                  WLAN: ${phone.others ? phone.others.WLAN : 'Not found'} <br>
+                  USB:  ${phone.others ? phone.others.USB : 'Not found'} <br>
+                  Bluetooth:  ${phone.others ? phone.others.Bluetooth : 'Not found'} <br>
+            </p>
+            
+        </div>
+    
+    `;
+    phoneDetail.appendChild(div);
 }
